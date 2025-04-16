91bcf7f9c74cc312164eeaf673825e15c9ee3924
mimihokuro <keetasan7@gmail.com>
2025-04-16 23:50:14 +0900

    🎨 フッターにサイトタイトルとコピーライト、リンクを追加

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,22 @@
-import { Stack, Text } from "@chakra-ui/react";
+import { Link, Stack, Text } from "@chakra-ui/react";
+
+const THIS_YEAR = new Date().getFullYear();
 
 const Footer = () => {
   return (
-    <Stack as="footer" backgroundColor="#444444" py={8}>
-      <Text fontSize={16} color="white">
-        &copy; mimihokuro
+    <Stack as="footer" py={8} gap={4}>
+      <Link
+        href="https://app.mimihokuro.com/"
+        _hover={{ textDecoration: "none" }}
+        fontSize={26}
+        fontWeight={600}
+        color={"#0AA864"}
+        lineHeight={1}
+      >
+        EC Tool Crate
+      </Link>
+      <Text as={"small"} fontSize={16}>
+        &copy; 2023-{THIS_YEAR} mimihokuro. All Rights Reserved.
       </Text>
     </Stack>
   );
