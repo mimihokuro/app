a8adad9c0eeff453fa2c9577fc03392c561d60a4
mimihokuro <keetasan7@gmail.com>
2025-04-20 00:23:51 +0900

    🎨 コンテナクエリを使用するための属性追加と全体の包括スタイルの追加

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import TopPage from "../pages/TopPage";
 import HolidayCalculation from "../pages/HolidayCalculation";
+import { Stack } from "@chakra-ui/react";
 
 const router = createBrowserRouter([
   {
@@ -18,5 +19,27 @@ const router = createBrowserRouter([
 ]);
 
 export const Routes = () => {
-  return <RouterProvider router={router} />;
+  return (
+    <Stack
+      width={"100%"}
+      p={{
+        base: 4,
+        md: "10",
+      }}
+      backgroundColor={"#ffffff"}
+      borderLeftRadius={{
+        base: "0px",
+        sm: "0px",
+        md: "32px",
+        lg: "32px",
+      }}
+      overflow={"hidden"}
+      sx={{
+        containerType: "inline-size",
+        containerName: "parent",
+      }}
+    >
+      <RouterProvider router={router} />;
+    </Stack>
+  );
 };
