bef9f6618420319931bbfe61cb522aa332a5c144
mimihokuro <keetasan7@gmail.com>
2025-04-27 22:40:03 +0900

    📝 テーマのセカンドカラーを変更

--- src/theme/index.jsx
+++ src/theme/index.jsx
@@ -3,7 +3,7 @@ import { extendTheme } from "@chakra-ui/react";
 const customTheme = extendTheme({
   colors: {
     primary: "#0AA864",
-    secondary: "#F44336",
+    secondary: "#47be8b",
   },
   fonts: {
     heading: `'Noto Sans JP', sans-serif`, // カスタムフォント
