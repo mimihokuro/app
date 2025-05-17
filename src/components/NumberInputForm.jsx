030cd3d0446a9d66d505e57898276dca610fc5f5
mimihokuro <keetasan7@gmail.com>
2025-05-17 23:58:59 +0900

    ♻️ 数値入力用Inputをコンポーネント

---
+++ src/components/NumberInputForm.jsx
@@ -0,0 +1,34 @@
+import {
+  FormControl,
+  FormLabel,
+  NumberInput,
+  NumberInputField,
+} from "@chakra-ui/react";
+import PropTypes from "prop-types";
+
+const NumberInputForm = ({ id, label, value, onChange }) => {
+  return (
+    <FormControl maxWidth={36}>
+      <FormLabel htmlFor={id}>{label}</FormLabel>
+
+      <NumberInput
+        id={id}
+        value={value}
+        onChange={onChange}
+        borderColor="#aaaaaa"
+        focusBorderColor="primary"
+      >
+        <NumberInputField />
+      </NumberInput>
+    </FormControl>
+  );
+};
+
+NumberInputForm.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+export default NumberInputForm;
